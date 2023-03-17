b0185aaad98a0835033631045e4a97136920f9e0
araelecs <aramirez48727@gmail.com>
2023-03-17 15:56:21 -0700

    fun with functions code along

---
+++ 011-functions/main.js
@@ -0,0 +1,12 @@
+//Rio thinks functions are fun :P
+
+function doMath (a, b) {    //this is the function signature
+    // let result = a * 4 NOT NEEDED //operator       //consider const instead of function in order to prevent it being overwritten
+    console.log(`The result is ${a * b}`) //instead of hard coding specific values, we can use variables for flexibiity
+    console.log(`The result is ${a / b}`)
+    console.log(`The result is ${a + b}`)
+    console.log(`The result is ${a - b}`)
+}
+
+doMath(100, 20) //must invoke/call the function for it to show up on the console.
+
