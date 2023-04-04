78d02ab093db028849b64f5081c96c0efc2cbfca
araelecs <aramirez48727@gmail.com>
2023-04-03 23:29:07 -0700

    my code for average trip cost

---
+++ 013-loops-arrays/README.md
@@ -0,0 +1,1 @@
+#I love devtools, I appreciate the fact that I can see the loop in action step by step
--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -1,5 +1,5 @@
 const dataMPG = [] //globally scoped, anything can access
-const dataTrip = []
+const dataCost = []
 const updateDOM = (input) => {
     const divEl = document.querySelector('#output') 
      const p = document.createElement('p')
@@ -14,12 +14,10 @@ const mileage = (miles, gallons, gasPrice = 4.95) => {
     updateDOM(` You purchased ${gallons} gallons of fuel at a price of ${gasPrice} and traveled a total of ${miles} miles.`)
     updateDOM(` Your current MPG is ${mpg}, your total cost is $${tripCost}.`)
     dataMPG.push(mpg) 
-    dataTrip.push(tripCost) //separate arrays = more flexible
+    dataCost.push(tripCost) //separate arrays = more flexible
 }
 
 const averageMPG = () => {
-//    const mpgAverage = (myArray[0] + myArray[2]) / 2 //will now show up until it is called
-//    updateDOM(` Your total average MPG is ${mpgAverage} MPG.`)
     let totalMPG = 0 //move this outside so that it reestablishes each time
     for (let i = 0; i < dataMPG.length; i++){//++ is incrementer or afterthought, it adds 1 every time //i < 10 will force a the first 10 values of the array to pass through i =(0-9) Will now change this to dataMPG.length so that it limits the loop to the number of values in the array, will not get NaN
         totalMPG = totalMPG + dataMPG[i] //is adding each value within array every time it loops, sick
@@ -29,6 +27,17 @@ const averageMPG = () => {
 }
 
  //calling function for it to updateDOM 
+//calculate average cost 
+//how would I do it? 
+//commit message : "my code for calc average cost"
+const averageCost = () => {
+    let totalCost = 0
+    for (let i = 0; i < dataCost.length; i++){
+        totalCost = totalCost + dataCost[i]
+    }
+    updateDOM(` Your total cost is $${totalCost}.`)
+}
+
 
 mileage(150, 10, 5.05)
 mileage(100, 20, 5.25)
@@ -38,3 +47,4 @@ mileage(250, 30, 5.95)
 mileage(115, 9, 5)
 
 averageMPG()
+averageCost()
