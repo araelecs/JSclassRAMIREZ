eb80bf6d845889277f8edf5076465b6405818ed0
araelecs <aramirez48727@gmail.com>
2023-04-22 15:32:02 -0700

    fixing code

--- 018-obj-min-globals-refactor/main.js
+++ 018-obj-min-globals-refactor/main.js
@@ -1,12 +1,11 @@
-//I tried to get rid of as many global consts as possible by refactoring them into objects, but I still need a bit more help getting the code to work
-//I'll watch your next video and fix it, thanks!
+//LETSGOOOOOO Thanks , Rio!
 
 const FORM = document.getElementById("form-input")
 const ERR = document.getElementById("err")
 
 const appInfo = {
-DATA_MPG: [], 
-DATA_COST: [],
+dataMPG: [], 
+dataCost: [],
 updateDOM: (input) => {
     const divEl = document.querySelector('#output') 
     const p = document.createElement('p')
@@ -17,26 +16,28 @@ updateDOM: (input) => {
         const mpg = Math.round(miles/gallons)
         const tripCost = Math.round(gallons * price)
         this.updateDOM(` You purchased ${gallons} gallons of fuel at a price of ${price} and traveled a total of ${miles} miles.`)
-        this.DATA_MPG.push(mpg) 
-        this.DATA_COST.push(tripCost) //we can use the this as this is going to refer to the local scope code (which is pretty much everything now)
+        this.dataMPG.push(mpg) 
+        this.dataCost.push(tripCost) //we can use the this as this is going to refer to the local scope code (which is pretty much everything now)
         this.updateDOM(` Your current MPG is ${mpg}, your total cost is $${tripCost}.`)
     },
-    CALC_SUM: (arr) => { //it's nice to get rid of all the "const" declarations, makes the code look a bit neater
+    calcSum: (arr) => { //it's nice to get rid of all the "const" declarations, makes the code look a bit neater
         let sum = 0
         for(value of arr) {
         sum += (value) 
    }
         return sum 
     },
-    AVERAGE_CALC: function() {
-    const totalMPG = this.CALC_SUM(this.DATA_MPG)
-    const totalCost = this.CALC_SUM(this.DATA_COST)    
-    const averageMPG = Math.round(totalMPG/this.DATA_MPG.length)
-    const AVERAGE_COST = Math.round(totalCost/this.DATA_COST.length)
+    averageCalc: function() {
+    const totalMPG = this.calcSum(this.dataMPG)
+    const totalCost = this.calcSum(this.dataCost)    
+    const averageMPG = Math.round(totalMPG/this.dataMPG.length)
+    const AVERAGE_COST = Math.round(totalCost/this.dataCost.length)
     this.updateDOM(` Your average MPG is ${averageMPG}.`)
     this.updateDOM(` Your average trip cost is $${AVERAGE_COST}.`)
 
 },
+
+
 formCheck: function (e) {
     const errorMessage = []
     const miles = parseInt(e.target.miles.value)
@@ -51,8 +52,8 @@ formCheck: function (e) {
         ERR.textContent = errorMessage 
     } else {
         ERR.textContent = ''
-        this.MILEAGE(miles, gallons, price) 
-        this.AVERAGE_COST()
+        this.mileageTrack(miles, gallons, price) 
+        this.averageCalc()
     }
     FORM.reset()   
     }
@@ -63,20 +64,3 @@ FORM.addEventListener('submit', (e) => {
     e.preventDefault()
     appInfo.formCheck(e)
 })
-
-
-
-
-
-
-// const AVERAGE_COST = () => {
-//     let totalCost = 0
-//     for (let i = 0; i < DATA_COST.length; i++){          
-//         totalCost = totalCost + DATA_COST[i]
-//     }
-//     let AVERAGE_COST = (totalCost/DATA_COST.length)
-//     updateDOM(` Your average trip cost is $${Math.round(AVERAGE_COST)}.`) 
-// }
-
-
-
