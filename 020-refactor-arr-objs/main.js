457d9f548945516d32010fe055438ee15e1cd480
araelecs <aramirez48727@gmail.com>
2023-04-27 20:23:08 -0700

    refactor #2 arr objs

---
+++ 020-refactor-arr-objs/main.js
@@ -0,0 +1,83 @@
+const FORM = document.getElementById("form-input")
+const ERR = document.getElementById("err")
+const updateDOM = (input) => {
+    const divEl = document.querySelector('#output') 
+     const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p) 
+} 
+  
+  const APP_DATA = []
+
+  const INFO_TRACKER = (data) =>{
+    const mpg = Math.round(data.miles/data.gallons)
+    const tripCost = Math.round(data.gallons * data.price)
+    updateDOM(` You purchased ${gallons} gallons of fuel at a price of ${price} and traveled a total of ${miles} miles.`)
+    updateDOM(` Your current MPG is ${mpg}, your total cost is $${tripCost}.`)
+}
+    
+  
+ const DATA_MPG = [] 
+const DATA_COST = []
+const MILEAGE = (miles, gallons, price = 4.95) => {
+    
+    DATA_MPG.push(mpg) 
+    DATA_COST.push(tripCost) 
+}
+
+const CALC_SUM = (arr) => {
+     let sum = 0
+     for(value of arr) {
+     sum += (value)
+
+}
+    return sum 
+}
+
+const AVERAGE_CALC = () => {
+    let totalMPG = CALC_SUM(DATA_MPG)
+    let totalCost = CALC_SUM(DATA_COST)    
+    let averageMPG = Math.round(totalMPG/DATA_MPG.length)
+    let AVERAGE_COST = Math.round(totalCost/DATA_COST.length)
+    updateDOM(` Your average MPG is ${averageMPG}.`)
+    updateDOM(` Your average trip cost is $${AVERAGE_COST}.`)
+
+}
+
+const AVERAGE_COST = () => {
+    let totalCost = 0
+    for (let i = 0; i < DATA_COST.length; i++){          
+        totalCost = totalCost + DATA_COST[i]
+    }
+    let AVERAGE_COST = (totalCost/DATA_COST.length)
+    updateDOM(` Your average trip cost is $${Math.round(AVERAGE_COST)}.`) 
+}
+
+FORM.addEventListener('submit', (e) => { 
+    e.preventDefault() 
+    const errorMessage = []
+    const miles = parseInt(e.target.miles.value)
+    const gallons = parseInt(e.target.gallons.value) 
+    const price = parseInt(e.target.price.value) 
+    if(miles === 0 || gallons === 0 || price === 0){ 
+        errorMessage.push('Invalid Entry (0)') 
+    }
+    if(price > 1000) {
+        errorMessage.push(' Im Walking')
+    } if(errorMessage.length > 0) { 
+        ERR.textContent = errorMessage 
+    } else {
+        ERR.textContent = ''
+        const dataInput = {
+            miles:miles,
+            gallons:gallons,
+            price:price
+        }
+        // MILEAGE(miles, gallons, price) convert this
+        // AVERAGE_COST() 
+        
+    }
+    FORM.reset() 
+    
+})
+
