a8e98a4a3d6fc01e69b591a229df34f17845f325
araelecs <aramirez48727@gmail.com>
2023-05-15 05:22:44 -0700

    async code

--- 029-async-code/main.js
+++ 029-async-code/main.js
@@ -1,10 +1,63 @@
-let pizza
-function orderPizza() {
-    console.log('Order Pizza')
+// let pizza
+// function orderPizza() {
+//     console.log('Order Pizza')
+//     setTimeout(() => {
+//     pizza = '🍕'
+//     console.log(`${pizza} is ready`)
+// }, 2000)
+//     console.log('Pizza was ordered')
+// }
+// orderPizza()
+// console.log('Call Qoli')
+// console.log(`Eat ${pizza}`)
+
+function orderPizza(callback) {
     setTimeout(() => {
-    pizza = '🍕'
-}, 2000)
-    console.log('Pizza was ordered')
+        const pizza ='🍕'
+        callback()
+    },2000)
 }
-orderPizza()
-console.log(`Eat ${pizza}`)
+
+function pizzaReady(pizza) {
+    console.log(`Eat the ${pizza}`)
+}
+
+orderPizza(pizzaReady)
+console.log(`Call Qoli`)
+
+window.addEventListener('click', callback)
+
+function callback() {
+
+}
+
+function thing1() {
+    //Call Pizza Shop
+}
+
+function thing2() {
+    //Order the pizza
+}
+
+function thing3() {
+    //Eat the pizza
+
+}
+
+function thing1(callback) {
+    callback()
+}
+
+function thing2(callback) {
+    callback()
+}
+
+function thing3(callback) {
+    callback()
+}
+
+thing1(() => {
+    thing2(() => {
+        thing3()
+    })
+})
