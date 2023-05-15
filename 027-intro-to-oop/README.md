5c7a7f82c8d036f169605400e7274e872f065b1a
araelecs <aramirez48727@gmail.com>
2023-05-15 00:03:02 -0700

    my attempt at oop coding challenge

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -1,3 +1,9 @@
 #Intro to OOP
+
 #Hey Rio, I understand the difference in syntax, but I still don't see why we would use this instead of our original method. I guess I will have to see when we start to include it in the code later on.
+
 #Student class subclass and the subtype chain make a bit more sense to me now. I can definitely see this way being a lot more organized than our original method. I can see this being useful when referencing specific categories in a big app. I assume this is one way to be a bit more organized and allow devs to target more objects. 
+
+#For the challenge, I was able to set getters and setters. I did figure out that we did not get "undefined" for our first name as we had established and used a getter and setter. I then added getters to the remaining elements of our console.log call and it eliminated the undefined error. However, now it displays code instead. I think this is a step towards the right direction though
+
+#
--- 027-intro-to-oop/main.js
+++ 027-intro-to-oop/main.js
@@ -1,24 +1,43 @@
 class Person { // in a class we must cap the first letter of the name
     constructor(firstName, lastName, address){
-        this.firstName = firstName //we use = instead of the : we are used to
-        this.lastName = lastName
-        this.address = address
+        this._firstName = firstName; //we use = instead of the : we are used to
+        this._lastName = lastName;
+        this._address = address; //underscore = private
     }
-    //private vs public fields
+    //private vs public fields //getter setter, one sets value, one gets the value
 
-    showStatus(){
-        console.log(`${this.firstName} lives at ${this.address}`)
+    getFirstName() {
+        return `${this._firstName}` //getter
+    }
+    setFirstName(newFirstName) {
+        this._firstName = newFirstName //setter
+    }
+    getLastName() {
+        return `${this._lastName}` //getter
+    }
+    setLastName(newLastName) {
+        this._LastName = newLastName //setter
+    }
+    getAddress() {
+        return `${this._address}` //getter
+    }
+    setAddress(newAddress) {
+        this._address = newAddress //setter
+    }
+    showStatus() {
+        console.log(`${this.getFirstName()} lives at ${this.getAddress}`)
     }
 }
 
-class Student extends Person{
+class Student extends Person {
     constructor(firstName, lastName, address, classList) {
         super(firstName, lastName, address)
         this.classList = classList
     }
     showStatus() {
-        console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`)
+        console.log(`${this.getFirstName} lives at ${this.getAddress} is taking ${this.classList}`)
     }
+
 }
 
 const anthony = new Student("Anthony", "Ramirez", "House Street", ["English1", "CIT45", "CIT58"]) //new object created from a template/blueprint which includes both data and methods
@@ -26,5 +45,5 @@ anthony.showStatus()
 const jimmy = new Person("Jimmy", "McGill", "IDK Blvd.")
 jimmy.showStatus()
 
-
+console.log(anthony.getFirstName())
 
