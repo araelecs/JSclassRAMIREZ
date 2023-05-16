7eb944816bdb260de5f4362346f10a687f0d20cf
araelecs <aramirez48727@gmail.com>
2023-05-15 22:09:49 -0700

    fetch header obj

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,11 +1,23 @@
-const url =  'https://www.thenewsapi.com'
-api_token= 
-'http://worldtimeapi.org/api/timezone/America/Los_Angeles'
+const url = 'https://banana.pudding'
 
+const request = new Request(url, {
+  headers: {
+    Authorization: "Bearer 123",
+  },
+});
 async function getData() {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(response)
+    try {
+        const response = await fetch(request)
+        const data = await response.json()
+        if(response.status !==200){
+        console.log('Success', data)
+        } else {
+            console.log('Server Error', data.error.message)
+        }
+        
+    }   catch(error) {
+        console.log('Fetch Error', error)
+    }
 }
 
 getData()
